8bdb5758d64ed68175d7f9d94b690d4e72091166
brunabdesouza <bruna.bdesouza@gmail.com>
2021-01-07 12:03:08 +1100

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ print "Creating flights..."
 
 Flight.destroy_all
 
-flight1 = Flight.create! number: 27, origin: 'Sydney', destination: 'Melbourne', airplane_id: airplane1.id
+flight1 = Flight.create! number: 27, origin: 'Sydney', destination: 'Melbourne', date: DateTime.strp-time('09/14/2021'), airplane_id: airplane1.id
 flight2 = Flight.create! number: 84, origin: 'Brisbane', destination: 'Adelaide', airplane_id: airplane2.id
 flight3 = Flight.create! number: 45, origin: 'Melbourne', destination: 'Tasmania', airplane_id: airplane3.id
 flight4 = Flight.create! number: 27, origin: 'Tasmania', destination: 'Sydney', airplane_id: airplane4.id
